c0b93d90a71666265fc6ebf6522be2c0e570215c
DESKTOP-BIT8QND\wnsgh <ths3630802@naver.com>
2022-01-10 23:39:20 +0900

    jwt 도입

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from rest_framework_jwt import obtain_jwt_token, refrech_jwt_token , verify_jwt_token
 from . import views
+
 
 
 urlpatterns = [
     path("signup/" , views.SignupView.as_view() , name = "signup"),
     path("<int:user_pk>/update/", views.ProfileUpdateAPI.as_view()),
+    path("token/" , obtain_jwt_token ),
+    path("token/refresh/" , refrech_jwt_token ),
+    path("token/verify/", verify_jwt_token),
 ]
