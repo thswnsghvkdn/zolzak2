b2eb68f6b3b779d7b9747db8b46adc75e5069adb
DESKTOP-BIT8QND\wnsgh <ths3630802@naver.com>
2022-01-12 21:46:38 +0900

    no message

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from rest_framework_jwt import obtain_jwt_token, refrech_jwt_token , verify_jwt_token
+from rest_framework_jwt import obtain_jwt_token, refresh_jwt_token , verify_jwt_token
 from . import views
 
 
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path("signup/" , views.SignupView.as_view() , name = "signup"),
     path("<int:user_pk>/update/", views.ProfileUpdateAPI.as_view()),
-    path("token/" , obtain_jwt_token ),
-    path("token/refresh/" , refrech_jwt_token ),
+    path("login/" , obtain_jwt_token ),
+    path("token/refresh/" , refresh_jwt_token ),
     path("token/verify/", verify_jwt_token),
 ]
